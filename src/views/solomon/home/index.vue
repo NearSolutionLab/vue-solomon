1771168a228cbb248bd50da8defee1eea5bc6af7
kang-h <toothed-roller0x@icloud.com>
2023-10-20 17:09:04 +0900

    wip: 홈화면 스타일 조정

--- src/views/solomon/home/index.vue
+++ src/views/solomon/home/index.vue
@@ -1,17 +1,14 @@
 <template>
-  <PageWrapper>
-    <div class="lg:flex">
-      <div class="lg:w-5/10 w-full !mr-4 enter-y">
-        <LatestDataList class="!my-4 enter-y" />
-      </div>
-      <div class="lg:w-5/10 w-full enter-y">
-        <UsageTable class="!my-4 enter-y" />
-      </div>
+  <div class="lg:flex px-4">
+    <div class="lg:w-5/10 w-full !mr-4 enter-y">
+      <LatestDataList class="!my-4 enter-y" />
     </div>
-  </PageWrapper>
+    <div class="lg:w-5/10 w-full enter-y">
+      <UsageTable class="!my-4 enter-y" />
+    </div>
+  </div>
 </template>
 <script lang="ts" setup>
-  import { PageWrapper } from '/@/components/Page';
   import LatestDataList from './components/LatestDataList.vue';
   import UsageTable from './components/UsageTable.vue';
 </script>
