f8ea28ae7779a148fdf10b7872d11d4c0ab1c26d
kang-h <toothed-roller0x@icloud.com>
2023-10-20 16:45:16 +0900

    wip: 로그인 화면 작업중

--- src/views/sys/login/LoginForm.vue
+++ src/views/sys/login/LoginForm.vue
@@ -1,65 +1,67 @@
 <template>
-  <LoginFormTitle v-show="getShow" class="enter-x" />
-  <Form
-    class="p-4 enter-x"
-    :model="formData"
-    :rules="getFormRules"
-    ref="formRef"
-    v-show="getShow"
-    @keypress.enter="handleLogin"
-  >
-    <FormItem name="account" class="enter-x">
-      <Input
-        size="large"
-        v-model:value="formData.account"
-        :placeholder="t('sys.login.userName')"
-        class="fix-auto-fill"
-      />
-    </FormItem>
-    <FormItem name="password" class="enter-x">
-      <InputPassword
-        size="large"
-        visibilityToggle
-        v-model:value="formData.password"
-        :placeholder="t('sys.login.password')"
-      />
-    </FormItem>
+  <div>
+    <LoginFormTitle v-show="getShow" class="enter-x" />
+    <Form
+      class="p-4 enter-x"
+      :model="formData"
+      :rules="getFormRules"
+      ref="formRef"
+      v-show="getShow"
+      @keypress.enter="handleLogin"
+    >
+      <FormItem name="account" class="enter-x">
+        <Input
+          size="large"
+          v-model:value="formData.account"
+          :placeholder="t('sys.login.userName')"
+          class="fix-auto-fill"
+        />
+      </FormItem>
+      <FormItem name="password" class="enter-x">
+        <InputPassword
+          size="large"
+          visibilityToggle
+          v-model:value="formData.password"
+          :placeholder="t('sys.login.password')"
+        />
+      </FormItem>
 
-    <ARow class="enter-x">
-      <ACol :span="12">
-        <FormItem>
-          <!-- No logic, you need to deal with it yourself -->
-          <Checkbox v-model:checked="rememberMe" size="small">
-            {{ t('sys.login.rememberMe') }}
-          </Checkbox>
-        </FormItem>
-      </ACol>
-      <ACol :span="12">
-        <FormItem :style="{ 'text-align': 'right' }">
-          <!-- No logic, you need to deal with it yourself -->
-          <Button type="link" size="small" @click="setLoginState(LoginStateEnum.RESET_PASSWORD)">
-            {{ t('sys.login.forgetPassword') }}
-          </Button>
-        </FormItem>
-      </ACol>
-    </ARow>
+      <ARow class="enter-x">
+        <ACol :span="12">
+          <FormItem>
+            <!-- No logic, you need to deal with it yourself -->
+            <Checkbox v-model:checked="rememberMe" size="small">
+              {{ t('sys.login.rememberMe') }}
+            </Checkbox>
+          </FormItem>
+        </ACol>
+        <ACol :span="12">
+          <FormItem :style="{ 'text-align': 'right' }">
+            <!-- No logic, you need to deal with it yourself -->
+            <Button type="link" size="small" @click="setLoginState(LoginStateEnum.RESET_PASSWORD)">
+              {{ t('sys.login.forgetPassword') }}
+            </Button>
+          </FormItem>
+        </ACol>
+      </ARow>
 
-    <FormItem class="enter-x">
-      <Button type="primary" size="large" block @click="handleLogin" :loading="loading">
-        {{ t('sys.login.loginButton') }}
-      </Button>
-      <!-- <Button size="large" class="mt-4 enter-x" block @click="handleRegister">
+      <FormItem class="enter-x">
+        <Button type="primary" size="large" block @click="handleLogin" :loading="loading">
+          {{ t('sys.login.loginButton') }}
+        </Button>
+        <!-- <Button size="large" class="mt-4 enter-x" block @click="handleRegister">
         {{ t('sys.login.registerButton') }}
       </Button> -->
-    </FormItem>
-    <ARow class="enter-x" :gutter="[16, 16]">
-      <ACol :md="24" :xs="24">
-        <Button block @click="setLoginState(LoginStateEnum.REGISTER)">
-          {{ t('sys.login.registerButton') }}
-        </Button>
-      </ACol>
-    </ARow>
-  </Form>
+      </FormItem>
+      <ARow class="enter-x" :gutter="[16, 16]">
+        <ACol :md="24" :xs="24">
+          <Button block @click="setLoginState(LoginStateEnum.REGISTER)">
+            {{ t('sys.login.registerButton') }}
+          </Button>
+        </ACol>
+      </ARow>
+    </Form>
+  </div>
 </template>
 <script lang="ts" setup>
   import { reactive, ref, unref, computed } from 'vue';
