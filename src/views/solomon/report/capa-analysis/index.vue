a3afbe1bcfc5e057c6805586eb607e263f9ed890
kang-h <toothed-roller0x@icloud.com>
2023-10-25 16:09:34 +0900

    feat(view): 주문 패턴 분석 리포트

--- src/views/solomon/report/capa-analysis/index.vue
+++ src/views/solomon/report/capa-analysis/index.vue
@@ -9,28 +9,26 @@
     </template>
     <div class="flex flex-col">
       <div class="flex-none h-96 flex flex-row pb-4">
-        <div class="flex-1 pr-4">
+        <div class="flex-none w-1/2 pr-4 h-90">
           <BasicTable @register="registerTable1" @row-click="rowClick" />
         </div>
-        <div ref="chartRef" class="flex-1"></div>
+        <div ref="chartRef" class="flex-none w-1/2 h-92"></div>
       </div>
-      <div class="flex-none h-96 flex flex-row">
-        <div class="flex-1">
-          <BasicTable @register="registerTable2">
-            <template #bodyCell="{ column, record }">
-              <template v-if="column.key === 'action'">
-                <TableAction
-                  :actions="[
-                    {
-                      icon: 'vscode-icons:file-type-excel',
-                      onClick: downloadReport.bind(null, record),
-                    },
-                  ]"
-                />
-              </template>
+      <div class="flex-none h-96">
+        <BasicTable @register="registerTable2">
+          <template #bodyCell="{ column, record }">
+            <template v-if="column.key === 'action'">
+              <TableAction
+                :actions="[
+                  {
+                    icon: 'vscode-icons:file-type-excel',
+                    onClick: downloadReport.bind(null, record),
+                  },
+                ]"
+              />
             </template>
-          </BasicTable>
-        </div>
+          </template>
+        </BasicTable>
       </div>
     </div>
   </PageWrapper>
@@ -38,12 +36,13 @@
 <script lang="ts" setup>
   import { PageWrapper } from '/@/components/Page';
   import { onMounted, ref, Ref } from 'vue';
-  import { GetCapaAnalysisReport } from '/@/api/solomon/report';
+  import { getCapaAnalysisReport, getDasCapaDetails } from '/@/api/solomon/report';
   import { useECharts } from '/@/hooks/web/useECharts';
   import { BasicTable, useTable, TableAction } from '/@/components/Table';
   import ReportHeader from '/@/views/solomon/report/components/ReportHeader.vue';
   import { formatNumber, stringToNumber } from '/@/utils/numberUtil';
   import { performancePerOrdersColumns, orderResultColumns } from './meta.data';
+  import { jsonToMultipleSheetXlsx } from '/@/components/Excel/src/Export2Excel';
 
   const [registerTable1, { setTableData: setTable1Data }] = useTable({
     title: '배치당 주문 수에 따른 주문처리 효율',
@@ -52,6 +51,8 @@
     showTableSetting: false,
     bordered: true,
     showIndexColumn: false,
+    pagination: false,
+    // scroll: { y: 100 },
     rowSelection: {
       type: 'radio',
     },
@@ -68,7 +69,7 @@
     isCanResizeParent: true,
     pagination: false,
     showSummary: true,
-    maxHeight: 200,
+    maxHeight: 180,
     summaryFunc: handleSummary,
     actionColumn: {
       width: 30,
@@ -117,11 +118,13 @@
   const chartRef = ref<HTMLDivElement | null>(null);
   const dataSetName = ref();
   const recommended = ref();
+  let requestId = null;
   const { setOptions } = useECharts(chartRef as Ref<HTMLDivElement>);
 
   onMounted(async () => {
     loadingRef.value = true;
-    const result = await GetCapaAnalysisReport(props.id);
+    const result = await getCapaAnalysisReport(props.id);
+    requestId = result.requestId;
     dataSetName.value = result.dataSetName;
     const [highestPerformance] = result.capaAnalysisList.sort(
       (a, b) => b.performanceRatio - a.performanceRatio,
@@ -197,7 +200,70 @@
     });
   });
 
-  function downloadReport(record: Recordable) {
-    console.log(record);
+  async function downloadReport(record: Recordable) {
+    if (requestId) {
+      loadingRef.value = true;
+      const { batchResult, combination, details } = await getDasCapaDetails(
+        requestId,
+        record.batchSize,
+        record.date,
+      );
+      const batchResultHeader = {
+        date: '날짜',
+        customerGroupNo: '배치 번호',
+        orderCount: '주문 수',
+        skuCount: '상품 수',
+        eaCountSum: '수량',
+      };
+      const combinationHeader = {
+        date: '날짜',
+        customerGroupNo: '배치 번호',
+        combination: '주문 조합',
+        orderCount: '주문 수',
+        eaCountSum: '수량',
+      };
+      const detailsHeader = {
+        date: '날짜',
+        customerGroupNo: '배치 번호',
+        orderNo: '주문 번호',
+        skuNo: '상품 번호',
+        skuName: '상품 이름',
+        eaCount: '수량',
+      };
+
+      jsonToMultipleSheetXlsx({
+        sheetList: [
+          {
+            data: batchResult as any,
+            header: batchResultHeader as any,
+            json2sheetOpts: {
+              // 순서
+              header: ['date', 'customerGroupNo', 'orderCount', 'skuCount', 'eaCountSum'],
+            },
+            sheetName: '배치결과 통계',
+          },
+          {
+            data: combination as any,
+            header: combinationHeader as any,
+            json2sheetOpts: {
+              // 순서
+              header: ['date', 'customerGroupNo', 'combination', 'orderCount', 'eaCountSum'],
+            },
+            sheetName: '배치별 조합 통계',
+          },
+          {
+            data: details as any,
+            header: detailsHeader as any,
+            json2sheetOpts: {
+              // 순서
+              header: ['date', 'customerGroupNo', 'orderNo', 'skuNo', 'skuName', 'eaCount'],
+            },
+            sheetName: '세부내역',
+          },
+        ],
+        filename: `주문패턴분석리포트_${record.date}.xlsx`,
+      });
+      loadingRef.value = false;
+    }
   }
 </script>
