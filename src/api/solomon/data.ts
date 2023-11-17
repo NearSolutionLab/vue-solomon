85e3f06b439d8e7ae9d3cc3e9a17a667c9b66bf7
seungwookchoi <tyvds2908@naver.com>
2023-11-17 16:44:28 +0900

    wip: 데이터 조회 화면 완료

--- src/api/solomon/data.ts
+++ src/api/solomon/data.ts
@@ -2,9 +2,27 @@ import { defHttp } from '/@/utils/http/axios';
 
 enum Api {
   GetDataSetList = '/data_lists',
+  GetOutBoundData = '/out_bound_inputs/',
+  GetInBoundData = '/in_bound_inputs/',
+  GetInventoryData = '/inventory_inputs/',
 }
 
 export const getDataSetList = async (params) => {
   const { total, items } = await defHttp.get({ url: Api.GetDataSetList, params });
+  return { total, items };
+};
+
+export const getOutBoundData = async (params) => {
+  const { total, items } = await defHttp.get({ url: Api.GetOutBoundData, params });
+  return { total, items };
+};
+
+export const getInBoundData = async (params) => {
+  const { total, items } = await defHttp.get({ url: Api.GetInBoundData, params });
+  return { total, items };
+};
+
+export const getInventoryData = async (params) => {
+  const { total, items } = await defHttp.get({ url: Api.GetInventoryData, params });
   return { total, items };
 };
---
+++ src/views/solomon/data/meta.data.ts
@@ -0,0 +1,265 @@
+import { BasicColumn, FormSchema } from '/@/components/Table';
+import { FormSchema } from '/@/components/Form';
+
+export const inboundColumns: BasicColumn[] = [
+  {
+    dataIndex: 'id',
+    ifShow: false,
+  },
+  {
+    title: '날짜',
+    dataIndex: 'date',
+    width: 100,
+  },
+  {
+    title: '고객',
+    dataIndex: 'orderNo',
+    width: 120,
+  },
+  {
+    title: 'SKU번호',
+    dataIndex: 'skuNo',
+    width: 120,
+  },
+  {
+    title: 'SKU이름',
+    dataIndex: 'skuName',
+    width: 120,
+  },
+  {
+    title: '수량',
+    dataIndex: 'eaCount',
+    width: 120,
+  },
+  {
+    title: '저장방식',
+    dataIndex: 'storageMethod',
+    width: 120,
+  },
+  {
+    title: '카테고리1',
+    dataIndex: 'category1',
+    width: 120,
+  },
+  {
+    title: '카테고리2',
+    dataIndex: 'category2',
+    width: 120,
+  },
+  {
+    title: '카테고리3',
+    dataIndex: 'category3',
+    width: 120,
+  },
+  {
+    title: '카테고리4',
+    dataIndex: 'category4',
+    width: 120,
+  },
+];
+
+export const outboundColumns: BasicColumn[] = [
+  {
+    dataIndex: 'id',
+    ifShow: false,
+  },
+  {
+    title: '날짜',
+    dataIndex: 'date',
+    width: 100,
+  },
+  {
+    title: '주문번호',
+    dataIndex: 'orderNo',
+    width: 120,
+  },
+  {
+    title: 'SKU번호',
+    dataIndex: 'skuNo',
+    width: 120,
+  },
+  {
+    title: 'SKU이름',
+    dataIndex: 'skuName',
+    width: 120,
+  },
+  {
+    title: '수량',
+    dataIndex: 'eaCount',
+    width: 120,
+  },
+  {
+    title: '저장방식',
+    dataIndex: 'storageMethod',
+    width: 120,
+  },
+  {
+    title: '카테고리1',
+    dataIndex: 'category1',
+    width: 120,
+  },
+  {
+    title: '카테고리2',
+    dataIndex: 'category2',
+    width: 120,
+  },
+  {
+    title: '카테고리3',
+    dataIndex: 'category3',
+    width: 120,
+  },
+  {
+    title: '카테고리4',
+    dataIndex: 'category4',
+    width: 120,
+  },
+];
+
+export const inventoryColumns: BasicColumn[] = [
+  {
+    dataIndex: 'id',
+    ifShow: false,
+  },
+  {
+    title: '날짜',
+    dataIndex: 'date',
+    width: 100,
+  },
+  {
+    title: '창고코드',
+    dataIndex: 'whCode',
+    width: 120,
+  },
+  {
+    title: '로케이션코드',
+    dataIndex: 'locCode',
+    width: 120,
+  },
+  {
+    title: 'SKU번호',
+    dataIndex: 'skuNo',
+    width: 120,
+  },
+  {
+    title: 'SKU이름',
+    dataIndex: 'skuName',
+    width: 120,
+  },
+  {
+    title: '수량',
+    dataIndex: 'eaCount',
+    width: 120,
+  },
+  {
+    title: '로케이션타입',
+    dataIndex: 'locType',
+    width: 120,
+  },
+  {
+    title: '재고타입',
+    dataIndex: 'invType',
+    width: 120,
+  },
+  {
+    title: '카테고리',
+    dataIndex: 'category',
+    width: 120,
+  },
+  {
+    title: '로트번호',
+    dataIndex: 'lotNo',
+    width: 120,
+  },
+  {
+    title: '유통기한',
+    dataIndex: 'expireDate',
+    width: 120,
+  },
+  {
+    title: '브랜드',
+    dataIndex: 'brand',
+    width: 120,
+  },
+];
+
+const colProps = {
+  span: 8,
+};
+
+export const outboundTestColumns: FormSchema[] = [
+  {
+    field: 'id',
+    component: 'Input',
+    colProps,
+  },
+  {
+    label: '날짜',
+    field: 'date',
+
+    component: 'Input',
+    colProps,
+  },
+  {
+    label: '고객',
+    field: 'orderNo',
+
+    component: 'Input',
+    colProps,
+  },
+  {
+    label: 'SKU번호',
+    field: 'skuNo',
+
+    component: 'Input',
+    colProps,
+  },
+  {
+    label: 'SKU이름',
+    field: 'skuName',
+
+    component: 'Input',
+    colProps,
+  },
+  {
+    label: '수량',
+    field: 'eaCount',
+
+    component: 'Input',
+    colProps,
+  },
+  {
+    label: '저장방식',
+    field: 'storageMethod',
+
+    component: 'Input',
+    colProps,
+  },
+  {
+    label: '카테고리1',
+    field: 'category1',
+
+    component: 'Input',
+    colProps,
+  },
+  {
+    label: '카테고리2',
+    field: 'category2',
+
+    component: 'Input',
+    colProps,
+  },
+  {
+    label: '카테고리3',
+    field: 'category3',
+
+    component: 'Input',
+    colProps,
+  },
+  {
+    label: '카테고리4',
+    field: 'category4',
+
+    component: 'Input',
+    colProps,
+  },
+];
