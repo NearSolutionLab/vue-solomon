4d3a7d9aee4f92d2f865a57ce2e35acd1cdddca8
kang-h <toothed-roller0x@icloud.com>
2023-11-27 16:51:09 +0900

    feat(style): 페이지 하단 영역 추가

--- src/layouts/default/footer/index.vue
+++ src/layouts/default/footer/index.vue
@@ -1,13 +1,22 @@
 <template>
   <Footer :class="prefixCls" v-if="getShowLayoutFooter" ref="footerRef">
+    <div>Near solution Copyright &copy; Near solution Inc. All Right Reserved. </div>
     <div :class="`${prefixCls}__links`">
-      <a @click="openWindow(SITE_URL)">{{ t('layout.footer.onlinePreview') }}</a>
-
-      <GithubFilled @click="openWindow(GITHUB_URL)" :class="`${prefixCls}__github`" />
-
-      <a @click="openWindow(DOC_URL)">{{ t('layout.footer.onlineDocument') }}</a>
+      <a-button size="small" type="link" @click="openSolomonPolicyRegisterModal">이용약관</a-button>
+      <a-button size="small" type="link" @click="openPrivacyPolicyRegisterModal"
+        >개인정보처리방침</a-button
+      >
     </div>
-    <div>Copyright &copy;2020 Vben Admin</div>
+    <SolomonPolicyModal
+      :showCancelBtn="false"
+      @register="SolomonPolicyRegister"
+      @ok="okSolomonPolicyModalHandler"
+    />
+    <PrivacyPolicyModal
+      :showCancelBtn="false"
+      @register="PrivacyPolicyRegister"
+      @ok="okPrivacyPolicyModalHandler"
+    />
   </Footer>
 </template>
 
@@ -15,9 +24,6 @@
   import { computed, defineComponent, unref, ref } from 'vue';
   import { Layout } from 'ant-design-vue';
 
-  import { GithubFilled } from '@ant-design/icons-vue';
-
-  import { DOC_URL, GITHUB_URL, SITE_URL } from '/@/settings/siteSetting';
   import { openWindow } from '/@/utils';
 
   import { useI18n } from '/@/hooks/web/useI18n';
@@ -25,10 +31,13 @@
   import { useRouter } from 'vue-router';
   import { useDesign } from '/@/hooks/web/useDesign';
   import { useLayoutHeight } from '../content/useContentViewHeight';
+  import SolomonPolicyModal from '/@/views/sys/login/SolomonPolicyModal.vue';
+  import PrivacyPolicyModal from '/@/views/sys/login/PrivacyPolicyModal.vue';
+  import { useModal } from '/@/components/Modal';
 
   export default defineComponent({
     name: 'LayoutFooter',
-    components: { Footer: Layout.Footer, GithubFilled },
+    components: { Footer: Layout.Footer, SolomonPolicyModal, PrivacyPolicyModal },
     setup() {
       const { t } = useI18n();
       const { getShowFooter } = useRootSetting();
@@ -48,15 +57,36 @@
         return unref(getShowFooter) && !unref(currentRoute).meta?.hiddenFooter;
       });
 
+      const [
+        SolomonPolicyRegister,
+        { openModal: openSolomonPolicyRegisterModal, closeModal: closeSolomonPolicyModal },
+      ] = useModal();
+
+      const [
+        PrivacyPolicyRegister,
+        { openModal: openPrivacyPolicyRegisterModal, closeModal: closePrivacyPolicyModal },
+      ] = useModal();
+
+      const okSolomonPolicyModalHandler = () => {
+        closeSolomonPolicyModal();
+      };
+
+      const okPrivacyPolicyModalHandler = () => {
+        closePrivacyPolicyModal();
+      };
+
       return {
         getShowLayoutFooter,
         prefixCls,
         t,
-        DOC_URL,
-        GITHUB_URL,
-        SITE_URL,
         openWindow,
         footerRef,
+        SolomonPolicyRegister,
+        openSolomonPolicyRegisterModal,
+        PrivacyPolicyRegister,
+        openPrivacyPolicyRegisterModal,
+        okSolomonPolicyModalHandler,
+        okPrivacyPolicyModalHandler,
       };
     },
   });
@@ -66,30 +96,11 @@
 
   @normal-color: rgba(0, 0, 0, 0.45);
 
-  @hover-color: rgba(0, 0, 0, 0.85);
+  @hover-color: rgba(255, 255, 255, 0.45);
 
   .@{prefix-cls} {
+    background-color: black;
     color: @normal-color;
     text-align: center;
-
-    &__links {
-      margin-bottom: 8px;
-
-      a {
-        color: @normal-color;
-
-        &:hover {
-          color: @hover-color;
-        }
-      }
-    }
-
-    &__github {
-      margin: 0 30px;
-
-      &:hover {
-        color: @hover-color;
-      }
-    }
   }
 </style>
